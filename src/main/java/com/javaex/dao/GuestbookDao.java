7615d24b126ea20662bf470f0791278a5749b62b
김민수 <minsu.kim1988@gmail.com>
2021-07-26 08:53:14 +0900

    25/July/2021 mybatis @Autowired @Repository

--- src/main/java/com/javaex/dao/GuestbookDao.java
+++ src/main/java/com/javaex/dao/GuestbookDao.java
@@ -1,14 +1,8 @@
 package com.javaex.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.sql.DataSource;
-
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -18,137 +12,25 @@ import com.javaex.vo.GuestbookVo;
 public class GuestbookDao {
 
 	@Autowired
-	private DataSource dataSource;
+	private SqlSession sqlSession; 
 	
-//field	
-	private Connection conn=null;
-	private PreparedStatement ps=null;
-	private ResultSet rs=null;
-	
-//connection	
-	private void getConnection() {
-		try {
-			
-			conn=dataSource.getConnection();
-		}catch (SQLException e) {
-			System.out.println("error"+e);
-		}
-		
-	}
-	
-//resource close
-	private void close() {
-		try {
-			if(rs!=null) {
-				rs.close();
-			}
-			if(ps!=null) {
-				ps.close();
-			}
-			if(conn!=null) {
-				conn.close();
-			}
-		} catch(SQLException e) {
-			System.out.println("error"+e);
-		}
-	}
-	
+
 //insert
-	public int insert(GuestbookVo gb) {
-		int count = -1;
-		getConnection();
-		
-		try {
-			String query = "";
-			query += " insert into guestbook ";
-			query += " values( seq_no.nextval, ?, ?, ?, sysdate ) ";
-			ps=conn.prepareStatement(query);
-			ps.setString(1, gb.getName());
-			ps.setString(2, gb.getPassword());
-			ps.setString(3, gb.getContent());
-			
-			count = ps.executeUpdate();
-			
-			if(count>0) {
-				System.out.println(count+"건이 등록됨");
-			}else {
-				System.out.println("관리자 문의");
-			}
-			
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-		close();
+	public int insert(GuestbookVo gbVo) {
+
+		int count = sqlSession.insert("guestbook.insert", gbVo);
 		return count;
 	}
 	
 //delete
-	public int delete(GuestbookVo gb) {
-		int count = -1;
-		getConnection();
-		
-		try {
-			String query = "";
-			query+= " delete from guestbook ";
-			query+= " where no = ? ";
-			query+= " and password = ? ";
-			
-			ps=conn.prepareStatement(query);
-			ps.setInt(1, gb.getNo());
-			ps.setString(2, gb.getPassword());
-			
-			count = ps.executeUpdate();
-			
-			if(count>0) {
-				System.out.println(count+"건이 삭제됨");
-			}else {
-				System.out.println("관리자문의");
-			}
-			
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-		close();
+	public int delete(GuestbookVo gbVo) {
+		int count = sqlSession.delete("guestbook.delete", gbVo);
 		return count;
 	}
 	
 //list	
-	
-	
 	public List<GuestbookVo> getList(){
-		List<GuestbookVo> guestbookList = new ArrayList<GuestbookVo>();
-		
-		getConnection();
-		
-		try {
-			String query = "";
-			query += " select no, ";
-			query += "        name, ";
-			query += "        password, ";
-			query += "        content, ";
-			query += "        reg_date ";
-			query += " from guestbook ";
-			query += " order by no ASC ";
-			
-			ps = conn.prepareStatement(query);
-			rs=ps.executeQuery();
-			while(rs.next()) {
-				int no = rs.getInt("no");
-				String name = rs.getString("name");
-				String password = rs.getString("password");
-				String content = rs.getString("content");
-				String regDate = rs.getString("reg_date");
-				
-				GuestbookVo guestbookVo = new GuestbookVo(no, name, password, content, regDate);
-				
-				guestbookList.add(guestbookVo);
-			}
-			
-			
-		} catch(SQLException e) {
-			e.printStackTrace();
-		}
-		close();
+		List<GuestbookVo> guestbookList = sqlSession.selectList("guestbook.selectList");
 		return guestbookList;
 	}
 }
